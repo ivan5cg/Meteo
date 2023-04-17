351c017edb91f10fdc0ff86d85d454a0439b1957
ivan5cg <66747803+ivan5cg@users.noreply.github.com>
2023-04-17 19:01:41 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -84,7 +84,6 @@ def get_temp_data(valid_run):
     return temp_data
 
 
-
 def get_wind_gust_data(valid_run):
 
     url ='https://www.meteociel.fr/modeles/pe-arome_table.php?x=0&y=0&lat=40.41&lon=-3.658&mode=13&sort=0'
@@ -94,7 +93,18 @@ def get_wind_gust_data(valid_run):
 
     return wind_gust_data
 
+def get_pressure_data(valid_run):
 
+    url ='https://www.meteociel.fr/modeles/pe-arome_table.php?x=0&y=0&lat=40.41&lon=-3.658&mode=1&sort=0'
+    url_run = f'{url}&run={valid_run}'
+
+    pressure_data = get_arome_data(url_run)
+
+    return pressure_data
+
+
+
+#####################################################
 
 
 temp_data = get_temp_data(valid_run)
@@ -187,8 +197,6 @@ def plot_temp_data(data):
         ax.set_xticklabels(tick_labels, fontsize=10, rotation=0, ha='center')
 
         return 
-
-
 
 st.pyplot(plot_temp_data(temp_data))
 
@@ -285,3 +293,96 @@ def plot_wind_data(data):
 
 st.pyplot(plot_wind_data(wind_data))
 
+#@#############################################
+
+
+pressure_data = get_pressure_data(valid_run)
+
+def plot_pressure_data(data):
+
+        data = data
+
+        # Set figure size and resolution
+        fig, ax = plt.subplots(figsize=(10, 6), dpi=100)
+
+        # Set plot style
+        plt.style.use('ggplot')
+
+        # Iterate over the columns and plot each one
+        for column in data.columns[:-1]:
+            ax.plot(data.index, data[column], alpha=0.9)
+
+        #ax.plot(data["Actual data"], alpha=1,linewidth=4,color="black")
+
+        # Add title and labels
+
+
+        plt.title('Pressure Forecast for the next 2 days', fontsize=16)
+        plt.xlabel('Date', fontsize=12)
+        plt.ylabel('Pressure (hpa)', fontsize=12)
+
+       
+
+        # Remove top and right spines
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+
+        # Set x-axis tick parameters
+        plt.xticks(fontsize=10, rotation=0, ha='right')
+
+        # Set y-axis tick parameters
+        plt.yticks(fontsize=10)
+
+        # Add vertical lines for each hour
+        for hour in data.index:
+            ax.axvline(hour, linestyle='--', color='black', alpha=0.1)
+
+        # Remove gridlines
+        plt.grid(True)
+
+        # Compute the minimum and maximum temperature for each day and their respective indexes
+        dates = list(set(data.index.date))
+        min_temps = []
+        max_temps = []
+        min_idx = []
+        max_idx = []
+
+        for date in dates:
+            df = data.loc[data.index.date == date]
+            min_temp = df.min().min()
+            max_temp = df.max().max()
+            min_idx.append(data.loc[data.index.date == date].idxmin().min())
+            max_idx.append(data.loc[data.index.date == date].idxmax().min())
+            min_temps.append(min_temp)
+            max_temps.append(max_temp)
+
+        # Add the minimum temperature text to the plot
+        for i, temp in enumerate(min_temps):
+            min_temp = "{:.0f}".format(temp)
+            ax.text(min_idx[i], temp, min_temp, ha='left', va='top', color='blue',fontweight="bold")
+
+        # Add the maximum temperature text to the plot
+        for i, temp in enumerate(max_temps):
+            max_temp = "{:.0f}".format(temp)
+            ax.text(max_idx[i], temp, max_temp, ha='left', va='bottom', color='red',fontweight="bold")
+
+
+        # Format x-axis ticks
+        # Format x-axis ticks
+        ticks = []
+        tick_labels = []
+        for date in data.index:
+                if date.hour == 0:
+                    tick_labels.append(date.strftime('%a, %b %d'))
+                    ticks.append(date)
+                if date.hour % 6 == 0:
+                    tick_labels.append(date.strftime('%H'))
+                    ticks.append(date)
+                    pass
+
+        ax.set_xticks(ticks)
+        ax.set_xticklabels(tick_labels, fontsize=10, rotation=0, ha='center')
+
+        return 
+
+st.pyplot(plot_pressure_data(pressure_data))
