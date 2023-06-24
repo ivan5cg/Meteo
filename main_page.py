a517fdbba5dab3e67336b360d1cdc97a239ce1b3
ivan5cg <66747803+ivan5cg@users.noreply.github.com>
2023-06-24 16:34:08 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -191,10 +191,10 @@ desv_temp = temp_data.loc[temp_data.index[(temp_data.index.hour==hora) & (temp_d
 
 col1,col2,col3 = st.columns(3)
 
-col1.metric("Temperatura actual (ºC)",temp_actual,temp_actual-temp_ayer,delta_color="inverse")
-col2.metric("Temperatura mañana (ºC)",temp_mañana,desv_temp,delta_color="off")
+col1.metric("Temperatura actual (ºC)",temp_actual,(temp_actual-temp_ayer).round(1),delta_color="inverse")
+col2.metric("Temperatura mañana (ºC)",temp_mañana,(temp_mañana-temp_actual),delta_color="inverse")
 
-st
+
 
 ########################################################
 
