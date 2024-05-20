cccbddc39d53ea967dcb864e7bf95a2b5c89ab44
ivan5cg <ivan.carrillo.g5@gmail.com>
2024-05-20 19:52:19 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1229,6 +1229,8 @@ def plot_sun_elevation(latitude, longitude, timezone_str='UTC'):
 st.pyplot(plot_sun_elevation(40.41144776110279, -3.6787949052050672, 'Europe/Madrid'))
 
 
+st.divider()
+
 
 # URLs of the images
 image_urls = [
