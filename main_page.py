a43e221c80eba526d88a42b7225b15134cec3ad5
ivan5cg <ivan.carrillo.g5@gmail.com>
2024-06-26 21:02:42 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -391,16 +391,6 @@ else:
 
 
 
-try:
-    loop = asyncio.get_running_loop()
-except RuntimeError:
-    loop = None
-
-if loop and loop.is_running():
-    asyncio.ensure_future(main())
-else:
-    asyncio.run(main())
-    
 
 
 
