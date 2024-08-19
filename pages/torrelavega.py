788377ca587e3296d195d2b01e0a53854e80a50b
ivan5cg <ivan.carrillo.g5@gmail.com>
2024-08-19 20:17:50 +0200

    Update torrelavega.py

--- pages/torrelavega.py
+++ pages/torrelavega.py
@@ -309,7 +309,6 @@ def plot_temp_data(data):
             max_temp = "{:.1f}".format(temp)
             ax.text(max_idx[i], temp, max_temp, ha='left', va='bottom', color='red',fontweight="bold")
 
-
         max_usual_temp_upper = 22.5
         max_usual_temp_lower = 25.5
 
@@ -320,9 +319,6 @@ def plot_temp_data(data):
 
         ax.fill_between(data.index,min_usual_temp_upper,min_usual_temp_lower, alpha=0.2, color='blue')
 
-
-
-        # Format x-axis ticks
         # Format x-axis ticks
         ticks = []
         tick_labels = []
@@ -824,13 +820,13 @@ def plot_long_forecast():
                 whiskerprops=whiskerprops,flierprops=flierprops,medianprops=medianprops);
 
 
-    max_usual_temp_upper = 30
-    max_usual_temp_lower = 27
+    max_usual_temp_upper = 22.5
+    max_usual_temp_lower = 25.5
 
     ax.fill_between(data.index,max_usual_temp_upper,max_usual_temp_lower, alpha=0.2, color='red')
 
-    min_usual_temp_upper = 20
-    min_usual_temp_lower = 17
+    min_usual_temp_upper = 15
+    min_usual_temp_lower = 18
 
     ax.fill_between(data.index,min_usual_temp_upper,min_usual_temp_lower, alpha=0.2, color='blue')
 
