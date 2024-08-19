0ee1e4a5b8f9388b62e628fad2fe393de9dea306
ivan5cg <ivan.carrillo.g5@gmail.com>
2024-08-19 20:10:03 +0200

    Update alicante.py

--- pages/alicante.py
+++ pages/alicante.py
@@ -688,7 +688,7 @@ st.divider()
 
 @st.cache_data(ttl=60*60)
 def get_forecast_data():
-     data = pd.read_json("https://api.open-meteo.com/v1/forecast?latitude=40.41&longitude=-3.659&hourly=temperature_2m,precipitation,pressure_msl,cloudcover,windspeed_10m,windgusts_10m,cape&current_weather=true&timezone=Europe%2FBerlin&past_days=1&models=ecmwf_ifs04,gfs_global,icon_eu,meteofrance_arpege_europe,meteofrance_arome_france_hd")
+     data = pd.read_json("https://api.open-meteo.com/v1/forecast?latitude=38.346&longitude=-0.48&hourly=temperature_2m,precipitation,pressure_msl,cloudcover,windspeed_10m,windgusts_10m,cape&current_weather=true&timezone=Europe%2FBerlin&past_days=1&models=ecmwf_ifs04,gfs_global,icon_eu,meteofrance_arpege_europe,meteofrance_arome_france_hd")
      return data
 
 data = get_forecast_data()
